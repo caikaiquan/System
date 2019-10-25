2386d93487056d942a5cf0c12195c6fb212721ad
caikaiquan <caikaiquan@ule.com>
2019-10-25 15:47:22 +0800

    '编辑了两个demo'

--- src/main.js
+++ src/main.js
@@ -7,7 +7,12 @@ import '@/assets/css/base.scss'
 import '@/assets/css/public.css'
 // import '@/assets/css/styles.scss'
 import '@/assets/iconfont/iconfont.css'
+import Echarts from 'echarts'
+import EchartsGl from 'echarts-gl'
+
 Vue.config.productionTip = false
+Vue.prototype.$echarts = Echarts
+Vue.prototype.$echartsgl = EchartsGl
 
 new Vue({
   router,

69b4fd9e68fe60f694547f8fd6833e68a1437ded
caikaiquan <caikaiquan@ule.com>
2019-11-19 09:10:26 +0800

    11111

--- 1.js
+++ 1.js
@@ -4,7 +4,25 @@
 // console.log(reg.test('192.-1.1.1'));
 // console.log(reg.test('256.10.1.1'));
 
-let obj = { name:'123',age:18}
-for(let key in obj){
-    console.log(key,obj[key]);
+let obj = { name: '123', age: 18 }
+for (let key in obj) {
+    console.log(key, obj[key]);
 }
+
+
+let obj1 = {
+    "BeltLength": 0.0,
+    "BeltRPM": 0.0,
+    "BeltVelocity": 0.0,
+    "DrivenPulleyRPM": 0.0,
+    "DriverPulleyRPM": 0.0,
+    data: [
+        { val1: '1x', val2: '0.00', val3: '0.00', val4: '0.00', },
+        { val1: '2x', val2: '0.00', val3: '0.00', val4: '0.00', },
+        { val1: '3x', val2: '0.00', val3: '0.00', val4: '0.00', },
+        { val1: '4x', val2: '0.00', val3: '0.00', val4: '0.00', },
+    ],
+    "TransmissionRatio": 0.0
+}
+
+
